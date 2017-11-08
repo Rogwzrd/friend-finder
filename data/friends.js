88f7b395172550dbb0e88d5aaae449998debb3d2
Michael Daye <mgdaye@gmail.com>
2017-11-08 13:38:12 -0500

    filled in questions for survey and styled survey page further

--- data/friends.js
+++ data/friends.js
@@ -2,10 +2,12 @@
 var friends = [
     {
         name: "mike",
+        image: "link.com",
         answers: [ 1, 3, 4 ,5 , 2, 5, 4, 3, 2, 1]
     },
     {
         name: "julia",
+        image: "link.com",
         answers: [ 3, 2, 1, 5, 4, 5, 3, 4, 2, 2]
     }
 ];
