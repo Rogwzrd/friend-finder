73fba5d1f702a905c54f01c884afea8a1bd7346d
Michael Daye <mgdaye@gmail.com>
2017-11-09 14:55:19 -0500

    added slides to the carousel

--- data/friends.js
+++ data/friends.js
@@ -1,13 +1,13 @@
 //base friends
 var friends = [
     {
-        name: "mike",
-        image: "link.com",
+        name: "Mike",
+        image: "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-9/18157521_10208747502034534_6249058528946056879_n.jpg?oh=43ff60138eb1f1e28077876c1d9f84e3&oe=5A9B9B48",
         answers: [ 1, 3, 4 ,5 , 2, 5, 4, 3, 2, 1]
     },
     {
-        name: "julia",
-        image: "link.com",
+        name: "Julia",
+        image: "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-9/22309102_10210160321780563_5307791139212835425_n.jpg?oh=039675bad677a8da09912dfaa76cab48&oe=5AA85008",
         answers: [ 3, 2, 1, 5, 4, 5, 3, 4, 2, 2]
     }
 ];
