47cdad175acb801498e0b2ea51a009bf2d9fc4bd
Michael Daye <mgdaye@gmail.com>
2017-11-10 17:54:22 -0500

    refactoring the front page

--- data/friends.js
+++ data/friends.js
@@ -9,6 +9,97 @@ var friends = [
         name: "Julia",
         image: "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-9/22309102_10210160321780563_5307791139212835425_n.jpg?oh=039675bad677a8da09912dfaa76cab48&oe=5AA85008",
         answers: [ 3, 2, 1, 5, 4, 5, 3, 4, 2, 2]
+    },
+    {
+        name: "Kim",
+        image: "https://static.pexels.com/photos/301284/pexels-photo-301284.jpeg",
+        answers: [
+            "4",
+            "2",
+            "4",
+            "2",
+            "4",
+            "1",
+            "3",
+            "4",
+            "2",
+            "3"
+        ]
+    }, {
+        name: "Valarie",
+        image: "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/20770287_10101921204048948_8986384539618343936_n.jpg?oh=f7a9c2a8a14567032ff0a213c9c03f94&oe=5AA68F58",
+        answers: [
+            "4",
+            "4",
+            "4",
+            "5",
+            "5",
+            "5",
+            "2",
+            "4",
+            "2",
+            "2"
+        ]
+    }, {
+        name: "Shahein",
+        image: "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/22852895_10102022641492498_7540494134726794580_n.jpg?oh=ff21e3c2fb2274b0407be43be3763621&oe=5A9F5225",
+        answers: [
+            "1",
+            "4",
+            "3",
+            "5",
+            "2",
+            "4",
+            "3",
+            "4",
+            "2",
+            "4"
+        ]
+    },{
+        name: "Gene",
+        image: "link.com",
+        answers: [
+            "5",
+            "2",
+            "3",
+            "4",
+            "5",
+            "3",
+            "4",
+            "4",
+            "1",
+            "5"
+        ]
+    },{
+        "name": "Julie Wilson",
+        "image": "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-1/14322636_1761610807420234_1946864614433004226_n.jpg?oh=b4c0daf699783d0b7fc4b8170510c7f3&oe=5A989DDF",
+        "answers": [
+            "5",
+            "5",
+            "3",
+            "3",
+            "2",
+            "2",
+            "3",
+            "3",
+            "3",
+            "2"
+        ]
+    },{
+        "name": "Musa",
+        "image": "musa",
+        "answers": [
+            "3",
+            "2",
+            "4",
+            "4",
+            "4",
+            "4",
+            "3",
+            "4",
+            "1",
+            "3"
+        ]
     }
 ];
 
